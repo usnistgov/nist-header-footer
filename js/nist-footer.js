f14b718b85298a2f9e904acaf554d444842c7a52
Jon Raedeke <jonraedeke@gmail.com>
2018-06-14 09:23:16 -0400

    Remove menu related js and keep the simple includes for both header and footer. This will still be handy if small changes are made.

--- js/nist-footer.js
+++ js/nist-footer.js
@@ -4,8 +4,9 @@
  *
  */
 
-
 $(document).ready(function(){
+
   $("body").append('<div id="nistfootergoeshere"></div>');
   $("#nistfootergoeshere").load("https://pages.nist.gov/nist-header-footer/boilerplate-footer.html");
+  
 });
